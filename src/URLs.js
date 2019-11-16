cf0d99515af6e4bad2a2e7bda69af277613a6823
huangyangquan <572518423@qq.com>
2019-11-16 16:11:41 +0800

    init all urls

---
+++ src/URLs.js
@@ -0,0 +1,9 @@
+export default {
+	baseURL: 'https://open.duyiedu.com',
+	findAll: '/api/student/findAll',
+	findByPage: '/api/student/findByPage',
+	delBySno: '/api/student/delBySno',
+	addStudent: '/api/student/addStudent',
+	updateStudent: '/api/student/updateStudent',
+	searchStudent: '/api/student/searchStudent'
+}
