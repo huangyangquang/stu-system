231185fe7520ee99df1c2d9dd597f8547b0b4e1a
huangyangquan <572518423@qq.com>
2019-11-18 19:56:09 +0800

    add toast Component

--- src/main.js
+++ src/main.js
@@ -2,11 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import toast from '@/tools/toast/index.js'
 
 Vue.config.productionTip = false
 
 // 使用eventBus来进行maskBox 和 stuList组件间通信
 Vue.prototype.bus = new Vue();
+
+Vue.prototype.$toast = toast
+
 
 new Vue({
   router,
